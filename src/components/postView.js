5b80015f24ed32a0baf73880a400716eda8a164f
selosele <osh3384@naver.com>
2021-02-08 16:16:20 +0900

    import 모듈별로 구분지어서 줄바꿈

--- src/components/postView.js
+++ src/components/postView.js
@@ -1,4 +1,5 @@
 import React from "react"
+
 import { graphql, Link } from "gatsby"
 
 import Layout from "./layout"
--- src/components/profile.js
+++ src/components/profile.js
@@ -1,4 +1,5 @@
 import React from "react"
+
 import Image from "./image"
 import SearchInput from "../components/searchInput"
 
